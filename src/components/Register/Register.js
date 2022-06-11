4fbf9f5c51727d2fa12a33c3205f85a7a72838c8
Chirayu Shekkar <chirayu1901@hotmail.com>
2022-06-11 17:22:37 -0400

    Connected front end to back end using only Express

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,51 @@
 import React from 'react';
 
-const Register = ({ onRouteChange }) => {
+class Register extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      registerEmail: '',
+      registerPassword: '',
+      registerName: ''
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ registerEmail: event.target.value });
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({ registerPassword: event.target.value });
+  }
+
+  onNameChange = (event) => {
+    this.setState({ registerName: event.target.value });
+  }
+
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/register', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: this.state.registerEmail,
+        password: this.state.registerPassword,
+        name: this.state.registerName
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user) {
+          this.props.loadUser(user);
+          this.props.onRouteChange('home');
+        }
+      });
+  }
+
+  render() {
+    const {onRouteChange} = this.props;
     return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -10,6 +53,7 @@ const Register = ({ onRouteChange }) => {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                 <input
+                onChange={this.onNameChange}
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="text"
                   name="name"
@@ -19,6 +63,7 @@ const Register = ({ onRouteChange }) => {
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
+                onChange={this.onEmailChange}
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
@@ -28,6 +73,7 @@ const Register = ({ onRouteChange }) => {
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
+                onChange={this.onPasswordChange}
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
@@ -37,7 +83,7 @@ const Register = ({ onRouteChange }) => {
             </fieldset>
             <div className="">
               <input
-              onClick={() => onRouteChange('home')}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
@@ -47,6 +93,7 @@ const Register = ({ onRouteChange }) => {
         </main>
       </article>
     );
+  }
 }
 
 export default Register;
--- src/containers/App.js
+++ src/containers/App.js
@@ -72,7 +72,7 @@ const particleOptions = {
 }
 
 const app = new Clarifai.App({
-  apiKey: 'c75a43d1600748c2b19f7b332d224d2c'
+  apiKey: '9a2235bfd6954a58a1a770be5ba15dce'
 });
 
 class App extends Component {
@@ -83,10 +83,29 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
-  
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
   particlesInit = async (main) => {
     await loadFull(main);
   };
@@ -112,7 +131,7 @@ class App extends Component {
     this.setState({ input: event.target.value });
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     // console.log('click');
     this.setState({ imageUrl: this.state.input });
     app.models
@@ -120,23 +139,37 @@ class App extends Component {
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+            .catch(err => console.log(err));
+        }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
-      .catch(err => console.log(err));
+
   }
 
 
   onRouteChange = (route) => {
-    this.setState({route: route});
+    this.setState({ route: route });
     if (route === 'signout') {
-      this.setState({isSignedIn: false});
+      this.setState({ isSignedIn: false });
     } else if (route === 'home') {
-      this.setState({isSignedIn: true});
+      this.setState({ isSignedIn: true });
     }
   }
 
   render() {
-    const {isSignedIn, imageUrl, route, box} = this.state;
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className='App'>
         <Particles
@@ -145,22 +178,22 @@ class App extends Component {
           init={this.particlesInit}
           options={particleOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
-        { route === 'home'
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
+        {route === 'home'
           ? <div>
-              <Logo />
-              <Rank />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onPictureSubmit={this.onPictureSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
           : (
             route === 'signin'
-             ? <SignIn onRouteChange={this.onRouteChange}/>
-             : <Register onRouteChange={this.onRouteChange}/>
-            )
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          )
         }
       </div>
     );
